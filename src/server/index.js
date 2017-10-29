394aa0d4481635e9dc087a666f8b715242437acd
Jose Marquez <joseangel2212@gmail.com>
2017-10-29 15:50:13 -0300

    Updated server

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', '..', '..', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', '..', '..', '..', 'index.html'));
 });
 
 app.listen(app.get('port'), (() => {
